1781ef63d7db32e93087f6531d23df106f9a429d
Josemi <josemijuanes@gmail.com>
2019-12-31 13:51:31 +0100

    docs: added script to generate documentation packages metadata

---
+++ packages/docs/build.js
@@ -0,0 +1,22 @@
+let fs = require("fs");
+let path = require("path");
+let docs = require("./package.json").packages;
+
+process.nextTick(function () {
+    //Build documentation packages data
+    let data = Object.keys(docs).map(function (key) {
+        let inputPath = path.resolve(process.cwd(), path.join("..", docs[key], "package.json"));
+        let pkg = JSON.parse(fs.readFileSync(inputPath, "utf8"));
+        return {
+            "name": pkg.name,
+            "description": pkg.description,
+            "version": pkg.version,
+            "path": key,
+            "type": "css toolkit"
+        };
+    });
+    //Write packages
+    let outputPath = path.join(process.cwd(), "data", "packages.json");
+    fs.writeFileSync(outputPath, JSON.stringify(data, null, "    "), "utf8");
+});
+
