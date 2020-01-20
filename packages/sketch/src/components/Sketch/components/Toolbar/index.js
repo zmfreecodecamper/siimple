59ad62dae703f0681b51335b1f630b315a413d1e
Josemi <josemijuanes@gmail.com>
2020-01-20 19:48:12 +0100

    sketch: init toolbar component

---
+++ packages/sketch/src/components/Sketch/components/Toolbar/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {ForEach, Renderer} from "@siimple/neutrine";
+import {Button} from "../Button/index.js";
+import style from "./style.scss";
+
+//Available items
+let availableItems = [
+    {"name": "selection", "icon": "pointer"},
+    {"name": "rectangle", "icon": "square"},
+    {"name": "ellipse", "icon": "circle"},
+    {"name": "line", "icon": "minus"},
+    {"name": "arrow", "icon": "arrow-right"},
+    {"name": "text", "icon": "text"}
+];
+
+//Export toolbar component
+export function Toolbar (props) {
+    return (
+        <div className={style.root}>
+            {/* Render elements */}
+            <ForEach items={availableItems} render={function (item, index) {
+                return React.createElement(Button, {
+                    "className": style.item,
+                    "active": props.currentElement === item.name,
+                    "icon": item.icon,
+                    "onClick": function () {
+                        return props.onElementClick(item.name);
+                    },
+                    "key": index
+                });
+            }} />
+            {/* Divider */}
+            <div className={style.divider} />
+            {/* Render grid option */}
+            <Renderer render={function () {
+                return React.createElement(Button, {
+                    "className": style.item,
+                    "active": props.gridActive,
+                    "icon": "grid",
+                    "onClick": props.onGridClick
+                });
+            }} />
+        </div>
+    );
+}
+
+
