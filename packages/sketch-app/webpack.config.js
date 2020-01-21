407850b6daf45cc0e75df4077c2e361ad2a99cfc
Josemi <josemijuanes@gmail.com>
2020-01-21 21:13:12 +0100

    sketch-app: init webpack.config.js

---
+++ packages/sketch-app/webpack.config.js
@@ -0,0 +1,135 @@
+//Import dependencies
+let path = require("path");
+
+//Import webpack plugins
+let MiniCssExtract = require("mini-css-extract-plugin");
+//let HtmlWebpackPlugin = require("html-webpack-plugin");
+let modulesPath = path.resolve(__dirname, "../../node_modules");
+let packagesPath = path.resolve(__dirname, "../");
+
+//Common loaders configuration
+let loaders = {
+    // Extract CSS styles to a separaate .css file
+    "cssExtract": {
+        "loader": MiniCssExtract.loader,
+        "options": {
+            "publicPath": "./"
+        }
+    },
+    // sass loader
+    "sass": {
+        "loader": "sass-loader",
+        "options": {
+            "includePaths": [modulesPath, packagesPath],
+            "implementation": require("sass")
+        }
+    },
+    // Common CSS loader for parsing .css and compiled .scss files
+    "css": {
+        "loader": "css-loader"
+    }
+};
+
+//Export the webpack configuration
+module.exports = function (env) {
+    //Export webpack configuration
+    return {
+        "entry": path.join(process.cwd(), "src", "index.js"),
+        "mode": "development",
+        "target": "web",
+        "output": {
+            "path": path.join(process.cwd(), "www"),
+            "filename": "app.js"
+        },
+        "resolve": {
+            "modules": [modulesPath, packagesPath],
+        },
+        "module": {
+            "rules": Object.values({
+                // Parse .scss files of this module and ignore .scss files of 
+                // other modules
+                "localScssLoader": {
+                    "test": /\.scss$/,
+                    "include": [
+                        path.resolve(__dirname, "./src"),
+                        path.resolve(__dirname, "../sketch/src")
+                    ],
+                    "use": Object.values({
+                        "cssExtractLoader": loaders.cssExtract,
+                        "cssLoader": {
+                            "loader": "css-loader",
+                            "options": {
+                                "modules": {
+                                    "mode": "local",
+                                    "localIdentName": "siimple-sketch__[hash:base64:8]"
+                                }
+                            }
+                        },
+                        "sassLoader": loaders.sass
+                    })
+                },
+                // Parse .scss files of the other modules
+                "modulesScss": {
+                    "test": /\.scss$/,
+                    "exclude": [
+                        path.resolve(__dirname, "./src"),
+                        path.resolve(__dirname, "../sketch/src")
+                    ],
+                    "use": Object.values({
+                        "cssExtractLoader": loaders.cssExtract,
+                        "cssLoader": loaders.css,
+                        "sassLoader": loaders.sass
+                    })
+                },
+                // @siimple/icons uses CSS instead of SCSS, so we need to add a loader
+                // for parsing css files
+                "cssLoader": {
+                    "test": /\.css$/,
+                    "use": Object.values({
+                        "cssExtractLoader": loaders.cssExtract,
+                        "cssLoader": loaders.css
+                    })
+                },
+                // Export images to img/ folder
+                "imageLoader": {
+                    "test": /\.(png|jpg|gif|svg)$/,
+                    "use": {
+                        "loader": "file-loader",
+                        "options": {
+                            "name": "[hash].[ext]",
+                            "outputPath": "img/"
+                        }
+                    }
+                },
+                // Export fonts to fonts/ folder
+                "fontLoader": {
+                    "test": /\.(ttf|woff|woff2)$/,
+                    "use": {
+                        "loader": "file-loader",
+                        "options": {
+                            "name": "[hash].[ext]",
+                            "outputPath": "fonts/"
+                        }
+                    }
+                },
+                // Parse JSX using babel
+                // BUT: ignore all .js files in node_modules and bower_components folders
+                "jsxLoader": {
+                    "test": /\.js$/,
+                    "include": [
+                        path.join(process.cwd(), "src"),
+                        path.resolve(process.cwd(), "../sketch/src")
+                    ],
+                    "exclude": /(node_modules|bower_components)/,
+                    "loader": "babel-loader"
+                }
+            })
+        },
+        "plugins": [
+            new MiniCssExtract({
+                "filename": "app.css"
+            })
+        ]
+    };
+};
+
