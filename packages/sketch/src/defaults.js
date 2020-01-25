ade296e36417c90f035f9c0fab0994ef04088c01
Josemi <josemijuanes@gmail.com>
2020-01-25 23:40:59 +0100

    sketch: added defaults

---
+++ packages/sketch/src/defaults.js
@@ -0,0 +1,34 @@
+//Export color list
+export const colors = {
+    "blue": "#4e91e4",
+    "mint": "#4ccdac",
+    "green": "#4acf7f",
+    "yellow": "#fbc850",
+    "red": "#ee675d",
+    "dark": "#34495e",
+    "light": "#eef2f7",
+    "transparent": "transparent"
+};
+
+//Export default colors
+export const selectionColor = "rgba(78, 145, 228, 0.1)";
+export const gridColor = "rgba(238, 242, 247, 1.0)"; 
+export const handlersColor = "rgba(27, 94, 177, 1.0)";
+
+//Text sizes
+export const fontSizes = {
+    "small": 14,
+    "medium": 20,
+    "large": 30,
+    "extra-large": 40
+};
+
+//Stroke widths
+export const strokes = {
+    "small": 2,
+    "medium": 4,
+    "large": 6,
+    "extra-large": 8
+};
+
+
