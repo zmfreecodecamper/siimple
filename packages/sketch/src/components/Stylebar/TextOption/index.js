5f57dfa9bebb00ce328f09107d2f2804e335c4de
Josemi <josemijuanes@gmail.com>
2020-01-25 13:36:00 +0100

    sketch: added text option component

---
+++ packages/sketch/src/components/Stylebar/TextOption/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {classNames} from "@siimple/neutrine";
+import {Renderer} from "@siimple/neutrine";
+import {Textarea} from "@siimple/neutrine";
+import {Option} from "../Option/index.js";
+
+//Export option component wrapper
+export function TextOption (props) {
+    return (
+        <Option title={props.title}>
+            <Renderer render={function () {
+                return React.createElement(Textarea, {
+                    "className": classNames({
+                        "siimple--bg-white": true,
+                        "siimple--px-1": true,
+                        "siimple--py-1": true
+                    }),
+                    "onChange": function (event) {
+                        console.log(event);
+                    },
+                    "rows": 3,
+                    "fluid": true,
+                    "defaultValue": props.value
+                });
+            }} />
+        </Option>
+    );
+}
+
