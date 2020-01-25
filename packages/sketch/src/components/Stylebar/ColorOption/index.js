16d97e793996388f95f788062dccd453c869c61c
Josemi <josemijuanes@gmail.com>
2020-01-25 13:10:04 +0100

    sketch: updated color picker option component

--- packages/sketch/src/components/Stylebar/ColorOption/index.js
+++ packages/sketch/src/components/Stylebar/ColorOption/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import {classNames} from "@siimple/neutrine";
 import {Renderer, ForEach, If} from "@siimple/neutrine";
 import {Icon} from "@siimple/neutrine";
-//import {color, invertColor, colorsList} from "../../../utils/colors.js";
 import {colors} from "../../../utils/style.js";
+import {Option} from "../Option/index.js";
 import style from "./style.scss";
 
 //Get text color
@@ -19,35 +19,33 @@ let getTextColor = function (name) {
 export function ColorOption (props) {
     let colorNames = Object.keys(colors);
     return (
-        <div className={style.root}>
-            <ForEach items={colorNames} render={function (name, index) {
-                //Build class list
-                let classStyle = classNames({
-                    [style.item]: true,
-                    [style.transparent]: name === "transparent"
-                });
-                //Handle color click
-                let handleClick = function () {
-                    return props.onChange(name); //Change color
-                };
-                //Build inline style
-                let inlineStyle = {
-                    "backgroundColor": colors[name],
-                    "color": getTextColor(name)
-                };
-                return (
-                    <div key={index} className={classStyle} onClick={handleClick} style={inlineStyle}>
-                        <If condition={name === props.value}>
-                            <Icon icon="check" className={style.icon} />
-                        </If>
-                    </div>
-                );
-            }} />
-        </div>
+        <Option title={props.title}>
+            <div className={style.root}>
+                <ForEach items={colorNames} render={function (name, index) {
+                    //Build class list
+                    let classStyle = classNames({
+                        [style.item]: true,
+                        [style.transparent]: name === "transparent"
+                    });
+                    //Handle color click
+                    let handleClick = function () {
+                        return props.onChange(name); //Change color
+                    };
+                    //Build inline style
+                    let inlineStyle = {
+                        "backgroundColor": colors[name],
+                        "color": getTextColor(name)
+                    };
+                    return (
+                        <div key={index} className={classStyle} onClick={handleClick} style={inlineStyle}>
+                            <If condition={name === props.value}>
+                                <Icon icon="check" className={style.icon} />
+                            </If>
+                        </div>
+                    );
+                }} />
+            </div>
+        </Option>
     );
 }
-
-ColorOption.defaultProps = {
-    "transparent": false
-};
 
