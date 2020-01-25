315f1a4173309978d343d95fdb8ed1b56af3a65b
Josemi <josemijuanes@gmail.com>
2020-01-25 13:07:16 +0100

    sketch: init size option component for stylebar

---
+++ packages/sketch/src/components/Stylebar/SizeOption/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import {classNames} from "@siimple/neutrine";
+import {Renderer, ForEach, If} from "@siimple/neutrine";
+import {Heading} from "@siimple/neutrine";
+import {Icon} from "@siimple/neutrine";
+import {Option} from "../Option/index.js";
+import style from "./style.scss";
+
+//List with all availbale sizes
+let sizes = ["small", "medium", "large"];
+
+//Icon sizes
+let iconSizes = {
+    "small": "16px",
+    "medium": "20px",
+    "large": "24px"
+};
+
+//Export Size picker option
+export function SizeOption (props) {
+    return (
+        <Option title={props.title}>
+            <div className={style.root}>
+                <ForEach items={sizes} render={function (name, index) {
+                    //Item class list
+                    let itemClass = classNames({
+                        [style.item]: true,
+                        [style.itemActive]: name === props.value
+                    });
+                    //Handle stroke click
+                    let handleClick = function () {
+                        return props.onChange(name); //Change size
+                    };
+                    return (
+                        <div key={index} className={itemClass} onClick={handleClick}>
+                            <Icon icon={props.icon} style={{"fontSize": iconSizes[name]}} />
+                        </div>
+                    );
+                }} />
+            </div>
+        </Option>
+    );
+}
+
+SizeOption.defaultProps = {
+    "title": null,
+    "icon": "minus"
+};
+
+
