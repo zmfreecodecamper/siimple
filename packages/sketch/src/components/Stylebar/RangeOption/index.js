fde251c53538ca6e0361bbaa2c2cf59efd89249a
Josemi <josemijuanes@gmail.com>
2020-01-26 22:13:55 +0100

    sketch: display current value in range option

--- packages/sketch/src/components/Stylebar/RangeOption/index.js
+++ packages/sketch/src/components/Stylebar/RangeOption/index.js
@@ -2,24 +2,32 @@ import React from "react";
 import {Renderer} from "@siimple/neutrine";
 import {Range} from "@siimple/neutrine";
 import {Option} from "../Option/index.js";
+import style from "./style.scss";
 
 //Export range option wrapper
 export function RangeOption (props) {
     return (
         <Option title={props.title}>
-            <Renderer render={function () {
-                return React.createElement(Range, {
-                    "onChange": function (event) {
-                        return props.onChange(Number(event.target.value));
-                    },
-                    "className": "siimple--mb-1 siimple--mt-1",
-                    "fluid": true,
-                    "min": props.min,
-                    "max": props.max,
-                    "step": props.step,
-                    "defaultValue": props.value
-                });
-            }} />
+            <div className={style.root}>
+                {/* Display range */}
+                <div className={style.range}>
+                    <Renderer render={function () {
+                        return React.createElement(Range, {
+                            "onChange": function (event) {
+                                return props.onChange(Number(event.target.value));
+                            },
+                            "className": "siimple--mb-1 siimple--mt-1",
+                            "fluid": true,
+                            "min": props.min,
+                            "max": props.max,
+                            "step": props.step,
+                            "defaultValue": props.value
+                        });
+                    }} />
+                </div>
+                {/* Display current value */}
+                <div className={style.value}>{props.value}</div>
+            </div>
         </Option>
     );
 }
