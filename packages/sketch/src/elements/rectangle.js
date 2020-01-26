98f9982e66d354d3d07a3084778cc3ef84ce0281
Josemi <josemijuanes@gmail.com>
2020-01-26 20:04:09 +0100

    sketch: fixed big with border-radius in rectangle element

--- packages/sketch/src/elements/rectangle.js
+++ packages/sketch/src/elements/rectangle.js
@@ -18,18 +18,19 @@ export const rectangleElement = {
         let yStart = getStartPosition(element.y, element.height); //Real y start position
         let xEnd = getEndPosition(element.x, element.width); //Real y end position
         let yEnd = getEndPosition(element.y, element.height); //Real y end position
+        let radius = Math.min(element.radius, Math.abs(element.width) / 2, Math.abs(element.height) / 2); //Get max radius
         context.beginPath();
         context.globalAlpha = element.opacity;
         //context.rect(element.x, element.y, element.width, element.height);
-        context.moveTo(xStart + element.radius, yStart);
-        context.lineTo(xEnd- element.radius, yStart);
-        context.quadraticCurveTo(xEnd, yStart, xEnd, yStart + element.radius);
-        context.lineTo(xEnd, yEnd - element.radius);
-        context.quadraticCurveTo(xEnd, yEnd, xEnd - element.radius, yEnd);
-        context.lineTo(xStart + element.radius, yEnd);
-        context.quadraticCurveTo(xStart, yEnd, xStart, yEnd - element.radius);
-        context.lineTo(xStart, yStart + element.radius);
-        context.quadraticCurveTo(xStart, yStart, xStart + element.radius, yStart);
+        context.moveTo(xStart + radius, yStart);
+        context.lineTo(xEnd - radius, yStart);
+        context.quadraticCurveTo(xEnd, yStart, xEnd, yStart + radius);
+        context.lineTo(xEnd, yEnd - radius);
+        context.quadraticCurveTo(xEnd, yEnd, xEnd - radius, yEnd);
+        context.lineTo(xStart + radius, yEnd);
+        context.quadraticCurveTo(xStart, yEnd, xStart, yEnd - radius);
+        context.lineTo(xStart, yStart + radius);
+        context.quadraticCurveTo(xStart, yStart, xStart + radius, yStart);
         context.closePath();
         context.fillStyle = colors[element.fillColor];
         context.fill();
