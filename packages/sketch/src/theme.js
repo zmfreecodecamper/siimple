a788305f4361acf5c6e08419f4fda644aab0633f
Josemi <josemijuanes@gmail.com>
2020-02-01 19:38:58 +0100

    sketch: added default theme

---
+++ packages/sketch/src/theme.js
@@ -0,0 +1,42 @@
+//Export default theme configuration
+export const theme = {
+    //Available colors
+    "colors": {
+        "blue": "#4e91e4",
+        "green": "#4acf7f",
+        "yellow": "#fbc850",
+        "red": "#ee675d",
+        "dark": "#34495e",
+        "light": "#eef2f7",
+        "white": "#ffffff",
+        "transparent": "transparent"
+    },
+    //Selection values
+    "selectionColor": "rgb(78, 145, 228)",
+    "selectionOpacity": 0.1,
+    //Defualt grid values
+    "gridColor": "rgb(238, 242, 247)", 
+    "gridWidth": 1, //Grid lines width
+    "gridOpacity": 1.0,
+    //Default resize values
+    "resizeColor": "rgb(27, 94, 177)",
+    "resizeWidth": 2, //Resize line width
+    "resizeOpacity": 1.0,
+    //Text sizes
+    "fontSizes": {
+        "small": 14,
+        "medium": 20,
+        "large": 30,
+        "extra-large": 40
+    },
+    "fontFamily": "'Nanum Pen Script'", 
+    //Stroke widths
+    "strokes": {
+        "small": 2,
+        "medium": 4,
+        "large": 6,
+        "extra-large": 8
+    }
+};
+
+
