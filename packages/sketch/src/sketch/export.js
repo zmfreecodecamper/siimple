c64138f9f313bc48853756503ac3fb710cc5ecb1
Josemi <josemijuanes@gmail.com>
2020-03-08 12:16:08 +0100

    sketch: added method to crop the exported png image

--- packages/sketch/src/sketch/export.js
+++ packages/sketch/src/sketch/export.js
@@ -17,11 +17,29 @@ export const exportFormats = {
     }
 };
 
+//Crop canvas
+//https://stackoverflow.com/a/13074780
+let cropCanvas = function (originalCanvas, options, callback) {
+    if (typeof options !== "object" || options === null) {
+        return originalCanvas.toBlob(callback);
+    }
+    //Get screenshot
+    let originalContext = originalCanvas.getContext("2d");
+    let image = originalContext.getImageData(options.x, options.y, options.width, options.height);
+    //Create a new canvas to draw the screenshot
+    let canvas = createCanvas(options.width, options.height); //New canvas
+    let context = canvas.getContext("2d");
+    context.putImageData(image, 0, 0);
+    return canvas.toBlob(function (blob) {
+        return callback(blob);
+    });
+};
+
 //Export sketch as PNG image
 export function exportAsPNG (sketch, options, callback) {
     //TODO: check the export format
     let elements = sketch.elements;
-    let canvas = createCanvas(options.width, options.height);
+    let canvas = createCanvas(sketch.width, sketch.height);
     canvas.style.display = "none"; //Hide canvas
     let context = canvas.getContext("2d"); //Get canvas context
     //Render the sketch
@@ -30,9 +48,10 @@ export function exportAsPNG (sketch, options, callback) {
         "selection": [] //No selection
     }));
     //Generate the PNG image
-    document.body.appendChild(canvas); //Append the canvas element
-    return canvas.toBlob(function (blob) {
-        document.body.removeChild(canvas); //Remove the canvas
+    //document.body.appendChild(canvas); //Append the canvas element
+    //return canvas.toBlob(function (blob) {
+    return cropCanvas(canvas, options.crop, function (blob) {
+        //document.body.removeChild(canvas); //Remove the canvas
         return callback(blob); //Return the blob
     });
 }
