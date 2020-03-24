dfe5eea7e613cf8c88cf138479d921639e329374
Josemi <josemijuanes@gmail.com>
2020-03-24 19:53:39 +0100

    sketch: minor fixes

--- packages/sketch/src/components/Menubar/index.js
+++ packages/sketch/src/components/Menubar/index.js
@@ -9,40 +9,64 @@ export class Menubar extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            "active": false
+            "active": false,
+            "current": ""
         };
         //Bind methods
         this.handleToggle = this.handleToggle.bind(this);
+        this.handleOptionChange = this.handleOptionChange.bind(this);
     }
     //Handle menubar toggle
     handleToggle() {
         return this.setState({
-            "active": !this.state.active
+            "active": !this.state.active,
+            "current": "" //Clear current option
         });
+    }
+    //Handle option change
+    handleOptionChange(name) {
+        let self = this;
+        return function () {
+            let current = self.state.current; //Get current value
+            return self.setState({
+                "current": (current === name) ? "" : name
+            });
+        };
     }
     //Render the menubar component
     render() {
         let self = this;
+        let current = this.state.current; //Current option
         return (
             <div className={style.root}>
-                <Renderer render={function () {
-                    return React.createElement(Button, {
-                        "className": style.item,
-                        "icon": (self.state.active) ? "cross" : "menu",
-                        "active": self.state.active,
-                        "onClick": self.handleToggle
-                    });
-                }} />
+                <div className={style.item}>
+                    <Renderer render={function () {
+                        return React.createElement(Button, {
+                            "className": style.item,
+                            "icon": (self.state.active) ? "cross" : "menu",
+                            "active": self.state.active,
+                            "onClick": self.handleToggle
+                        });
+                    }} />
+                </div>
                 {/* Render menu items */}
                 <If condition={this.state.active}>
                     {/* Settings button */}
-                    <If condition={this.props.showSettingsBtn} render={function () {
-                        return React.createElement(Button, {
-                            "className": style.item,
-                            "icon": "gear",
-                            "onClick": self.props.onSettingsClick
-                        });
-                    }} />
+                    <If condition={this.props.showSettingsBtn}>
+                        <div className={style.item}>
+                            <Renderer render={function () {
+                                return React.createElement(Button, {
+                                    "active": current === "settings",
+                                    "className": style.item,
+                                    "onClick": self.handleOptionChange("settings"),
+                                    "icon": "gear"
+                                });
+                            }} />
+                            <Dialog active={current === "settings"} orientation="right">
+                                Options
+                            </Dialog>
+                        </div>
+                    </If>
                     {/* Save button */}
                     <If condition={this.props.showSaveBtn} render={function () {
                         return React.createElement(Button, {
