2b82ae0a8b1194c8bea814cd6af66a971905fd91
Josemi <josemijuanes@gmail.com>
2020-04-05 17:39:36 +0200

    sketch: minor improvements in select option

--- packages/sketch/src/components/Option/SelectOption/index.js
+++ packages/sketch/src/components/Option/SelectOption/index.js
@@ -1,23 +1,18 @@
 import React from "react";
-import {classNames} from "@siimple/neutrine";
 import {Renderer, ForEach} from "@siimple/neutrine";
 import {Select} from "@siimple/neutrine";
 import {Option} from "../index.js";
 
 //Export option component wrapper
 export function SelectOption (props) {
+    //Handle select change
     let handleChange = function (event) {
         return props.onChange(event.target.value);
     };
-    let classList = classNames({
-        "siimple--bg-white": true,
-        "siimple--px-1": true,
-        "siimple--py-1": true
-    });
     //Return select option
     return (
         <Option title={props.title}>
-            <Select className={classList} fluid defaultValue={props.value} onChange={handleChange}>
+            <Select className="siimple--bg-white" fluid defaultValue={props.value} onChange={handleChange}>
                 <ForEach items={Object.keys(props.values)} render={function (key, index) {
                     return React.createElement("option", {
                         "value": key,
